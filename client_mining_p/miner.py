adeac605b6dd91b54c49decafc5cec1b166168a0
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-12-05 08:19:52 -0600

    i broke the code but its something

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,8 +4,9 @@ import requests
 import sys
 import json
 
-
+DIFFICULTY = 1
 def proof_of_work(block):
+
     """
     Simple Proof of Work Algorithm
     Stringify the block and look for a proof.
@@ -13,7 +14,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +32,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:DIFFICULTY] == "0" * DIFFICULTY
 
 
 if __name__ == '__main__':
@@ -38,10 +45,12 @@ if __name__ == '__main__':
         node = "http://localhost:5000"
 
     # Load ID
-    f = open("my_id.txt", "r")
-    id = f.read()
-    print("ID is", id)
-    f.close()
+    # f = open("t.txt", "r")
+    with open('my_id.txt') as f:
+        id=f.read()
+        print("ID is",id)
+        f.close()
+
 
     # Run forever until interrupted
     while True:
@@ -56,8 +65,8 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
-
+        new_proof = proof_of_work(data["last_block"])
+        
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
@@ -67,4 +76,10 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        coin_total = 0
+        if data:
+            coin_total += 1
+            print(f"New Block Forged") 
+            print(f"New Total Coins{coin_total}")
+        else:
+            print(f"Total Coins {coin_total}")
